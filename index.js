1b7de2be41565657779d4875a459fe102d2c96ac
Giacomo <143886528+Giac3eh@users.noreply.github.com>
2024-01-18 22:54:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-//quando clicco bottone se l'input è vuoto ci sarà l'alert sennò verrà creato un elemento li e verrà aggiunto come giflio a listContainer e poi il valore di inputBox verrà svuotato
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something");
@@ -17,7 +16,6 @@ function addTask(){
     saveData();
 };
 
-//ogni volta che cliccherò nel container se cliccherò sull'elemento "LI" aggiungerà la class CSS checked se cliccherò sullo "SPAN" rimuoverà l'elemento parente dello span (ovvero LI)
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
@@ -28,8 +26,6 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
-//per evitare che ogni volta che si refresha la pagina i dati vengano eliminati dobbiamo immagazzinarli 
-//salverò nel localStorage (ovvero il browser) tutti quelli Items facenti parte della listContainer sotto il nome di "data"
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
